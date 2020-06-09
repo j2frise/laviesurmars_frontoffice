ce3c411e06a06deb706408ce1bd7ebbb80d5f965
Sebastienbezombes <bezombessebastien@gmail.com>
2020-06-09 16:16:19 +0200

    init vue

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Team from '../views/Team.vue'
+
+Vue.use(VueRouter)
+
+// C'est dans la constante routes que vous définisez les différentes routes de votre projet
+// Chaque route doivent avoir au minimum un path et un component associé
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/team',
+    name: 'Team',
+    component: Team
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
