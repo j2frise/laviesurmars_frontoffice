d8404b4feff9dd0a6dd47656e7caaa9975fb887a
unknown <jarce2frise@yahoo.fr>
2020-06-18 23:41:03 +0200

    v-2

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  serverURI: 'http://e-cine.xyz/laviesurmars_backend/API',
+  serverURI: 'https://e-cine.xyz/laviesurmars_backend/API',
   fixedLayout: false,
   hideLogoOnMobile: false
 }
