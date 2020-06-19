44e8dc074c5b90b151f1a6c6581e599db32b8911
unknown <jarce2frise@yahoo.fr>
2020-06-19 09:28:18 +0200

    v-2

--- src/components/widgets/SiderbarLarge.vue
+++ src/components/widgets/SiderbarLarge.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
+<style lang="scss" scoped>
 .sidebar__large {
   z-index: 1000; // a changer
   color: white;
